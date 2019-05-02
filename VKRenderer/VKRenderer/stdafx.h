6c0db8ee2d9e379ac135bd84408cefc94a54886f
Rohit Tolety <rohit.tolety@digipen.edu>
2019-05-02 13:22:11 -0700

    Added libraries as dependencies and also a glfw window appears

--- VKRenderer/VKRenderer/stdafx.h
+++ VKRenderer/VKRenderer/stdafx.h
@@ -4,6 +4,14 @@
 #include <iostream>
 #include <stdlib.h>
 
+#define GLFW_INCLUDE_VULKAN
+#include "../Dependencies/GLFW/include/GLFW/glfw3.h"
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include "../Dependencies/GLM/glm/glm/vec4.hpp"
+#include "../Dependencies/GLM/glm/glm/mat4x4.hpp"
+
 
 
 //File Includes
